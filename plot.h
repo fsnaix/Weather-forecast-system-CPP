3a8be0c05dfbd0beeb92e0599ace16cda77e6013
fsnaix <truonggiangsn@gmail.com>
2024-01-29 09:52:49 +0800

    first commit

---
+++ plot.h
@@ -0,0 +1,13 @@
+#ifndef PLOT_H
+#define PLOT_H
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+#include "weather.h"
+void Plot(vector<City_report> alist);
+#endif
+
